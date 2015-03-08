bdf6b121ed2c5d360ed69424dfea66d16cd15e5c
Charlie Strawn <cdstrawn@gmail.com>
2015-03-08 12:26:15 -0500

    add simple tmux conf file

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,5 +3,20 @@ hash git 2> /dev/null || {
   exit
 }
 
+mkdir -p $HOME/tmp
+mkdir -p $HOME/tmp/vim
+mkdir -p $HOME/tmp/vim/swap
+mkdir -p $HOME/tmp/vim/backup
 
+files="aliases bashrc vimrc tmux.conf"
 
+mkdir -p $HOME/dotfiles_bak
+
+for file in $files; do
+	echo "backing up $file"
+	mv $HOME/.$file $HOME/dotfiles_bak
+	echo "linking $(pwd) to $HOME/.$file"
+	ln -s $(pwd)/$file $HOME/.$file
+done
+
+source $HOME/.bashrc
